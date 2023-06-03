4ca1179ced75dac6d01ec51bf252a70ee80789be
CinnamonrollSaigo <121988836+CinnamonrollSaigo@users.noreply.github.com>
2023-06-03 14:56:31 +0000

    [modify] add a parentheses

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def predict(prompt):
     return summary
 
 
-with gr.Blocks as demo:
+with gr.Blocks() as demo:
     textbox = gr.Textbox(placeholder="Enter you text to summarize", lines=4)
     gr.Interface(fn=predict, input=textbox, output="text")
 demo.launch()
